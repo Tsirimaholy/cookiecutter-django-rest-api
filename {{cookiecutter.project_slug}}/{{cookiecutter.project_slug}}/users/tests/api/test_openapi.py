c51d57613564612e9632f4087c267b9c4859fe66
Tsirimaholy <hei.tsirimaholy@gmail.com>
2025-03-28 15:04:03 +0300

    fix: OpenAPI tests to use JWT authentication

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/api/test_openapi.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/api/test_openapi.py
@@ -1,13 +1,22 @@
 from http import HTTPStatus
 
 import pytest
+from django.test.client import Client
 from django.urls import reverse
 
 
-@pytest.mark.skip("Find a way to authenticate as admin")
-def test_api_docs_accessible_by_admin(admin_client):
+def test_api_docs_accessible_by_admin(client: Client, admin_user):
+    # Create a token for admin user
+    jwt_create_url = reverse("jwt-create")
+    response = client.post(
+        jwt_create_url,
+        {"username": admin_user.username, "password": "password"},
+    )
+    token = response.data["access"]
+
+    # Use token to access API docs
     url = reverse("api-docs")
-    response = admin_client.get(url)
+    response = client.get(url, headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == HTTPStatus.OK
 
 
@@ -17,8 +26,17 @@ def test_api_docs_not_accessible_by_anonymous_users(client):
     response = client.get(url)
     assert response.status_code == HTTPStatus.UNAUTHORIZED
 
-@pytest.mark.skip("Find a way to authenticate as admin")
-def test_api_schema_generated_successfully(admin_client):
+
+def test_api_schema_generated_successfully(client: Client, admin_user):
+    # Create a token for admin user
+    jwt_create_url = reverse("jwt-create")
+    response = client.post(
+        jwt_create_url,
+        {"username": admin_user.username, "password": "password"},
+    )
+    token = response.data["access"]
+
+    # Use token to access API schema
     url = reverse("api-schema")
-    response = admin_client.get(url)
+    response = client.get(url, headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == HTTPStatus.OK
